038403d62b98c402915cc34c978007286ebf0fd9
Kanstantsin K <kk@tolkam.by>
2021-02-10 12:10:33 +0300

    added middleware for unhandled tags

---
+++ src/UnhandledTagsMiddleware.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace Tolkam\HTMLProcessor\Tags;
+
+use Tolkam\DOM\Manipulator\Manipulator;
+use Tolkam\HTMLProcessor\MiddlewareHandlerInterface;
+use Tolkam\HTMLProcessor\MiddlewareInterface;
+
+class UnhandledTagsMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var string
+     */
+    protected string $tagsPrefix;
+    
+    /**
+     * @param string $tagsPrefix
+     */
+    public function __construct(string $tagsPrefix = 'x-')
+    {
+        $this->tagsPrefix = $tagsPrefix;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function apply(
+        Manipulator $dom,
+        MiddlewareHandlerInterface $middlewareHandler
+    ): Manipulator {
+        // replace in body or first root
+        $xPathRoot = $dom->isHtmlDocument() ? '//body' : '//*[1]';
+        $xPath = $xPathRoot . "/*[starts-with(local-name(), '" . $this->tagsPrefix . "')]";
+        
+        $found = $dom->filterXPath($xPath);
+        if ($found->count()) {
+            $found->each(function (Manipulator $e) use ($dom) {
+                $comment = $dom->createComment('unhandled: ' . $e->nodeName());
+                $e->replaceWith($comment);
+            });
+        }
+        
+        return $dom;
+    }
+}
