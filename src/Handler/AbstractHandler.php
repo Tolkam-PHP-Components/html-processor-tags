fe00ef6fccf020ec73d5ee3b3f4aa789a6274ac0
Kanstantsin K <kk@tolkam.by>
2021-02-13 14:18:47 +0300

    removed abstract handler

--- src/Handler/AbstractHandler.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Tolkam\HTMLProcessor\Tags\Handler;
-
-abstract class AbstractHandler implements TagsHandlerInterface, ParametersAwareInterface
-{
-    use ParametersAwareTrait;
-}
